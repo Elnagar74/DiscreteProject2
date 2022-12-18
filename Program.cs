03062c345e09388f6b5aa255b123219180e894ad
Elnagar74 <115397929+Elnagar74@users.noreply.github.com>
2022-12-18 20:34:21 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿                        // محمود ابراهيم محمد النجار
+Console.Write("Enter The First Number: ");           
+double Start_Number =Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter The Second Number: ");
+double End_Number = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("----------------------------");
+Console.WriteLine("The Perfect Numbers Between {0} and {1}", Start_Number, End_Number);
+Console.WriteLine("----------------------------");
+
+double sum, m;
+
+for(; Start_Number < End_Number; Start_Number++)
+{
+    sum = 0;
+    for ( m = 1; m < Start_Number; m++)
+    {
+        if (Start_Number % m == 0)
+        { 
+        sum += m;
+        }
+    }
+
+    if (sum == Start_Number)
+    {
+    Console.WriteLine(sum);
+    }
+}
